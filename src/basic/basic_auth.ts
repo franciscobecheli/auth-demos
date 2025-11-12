6d89a678f5704883ecbe6ffd01d4c095e899f240
Francisco <franciscobecheli@hotmail.com>
2025-11-12 20:02:52 -0300

    feat: password hashing + refactor

---
+++ src/basic/basic_auth.ts
@@ -0,0 +1,89 @@
+import type {
+  FastifyInstance,
+  FastifyPluginOptions,
+  FastifyReply,
+  FastifyRequest,
+} from 'fastify';
+
+import {
+  type BasicAuthCredentials,
+  isCredentialsValid,
+} from '../credentials.js';
+
+export async function routes(
+  fastify: FastifyInstance,
+  _options: FastifyPluginOptions,
+) {
+  // Basic Auth has no /login endpoint. You have to pass the credentials via Authorization header to every protected resource you want to access. Then, every one of those endpoints must validate the credentials and deny/allow access
+  fastify.get('/basic/protected-resource', async (request, reply) => {
+    // This check is extremely important in production. It is disabled here because localhost runs in http (unprotected). Never allow http in production, you will be streaming your credentials for whoever wants to see.
+    // if (!isHttps(request)) {
+    //   return replyUnauthorized(reply);
+    // }
+
+    const credentials = parseAuthHeader(request);
+    if (!credentials) {
+      return replyUnauthorized(reply);
+    }
+
+    if (!(await isCredentialsValid(credentials))) {
+      return replyUnauthorized(reply);
+    }
+
+    return replyProtectedResource(reply);
+  });
+}
+
+// biome-ignore lint/correctness/noUnusedVariables: used only in real HTTPS environments
+const isHttps = (request: FastifyRequest) => {
+  return request.protocol === 'https';
+};
+
+// Parses Authorization header and returns credentials if valid. Returns null if the format is ivalid. Format must be username:password, base64 encoded in Basic Auth.
+const parseAuthHeader = (
+  request: FastifyRequest,
+): BasicAuthCredentials | null => {
+  const basicAuthHeader = request.headers.authorization;
+  if (!basicAuthHeader) {
+    return null;
+  }
+
+  const tokens = basicAuthHeader.split(' ');
+  if (
+    tokens.length !== 2 ||
+    tokens[0] !== 'Basic' ||
+    typeof tokens[1] !== 'string'
+  ) {
+    return null;
+  }
+
+  const base64Credentials = tokens[1];
+  const decodedCredentials = Buffer.from(base64Credentials, 'base64').toString(
+    'utf-8',
+  );
+
+  const credentialTokens = decodedCredentials.split(':');
+  if (
+    credentialTokens.length !== 2 ||
+    !credentialTokens[0] ||
+    !credentialTokens[1]
+  ) {
+    return null;
+  }
+
+  return { username: credentialTokens[0], password: credentialTokens[1] };
+};
+
+const replyUnauthorized = (reply: FastifyReply): FastifyReply => {
+  return reply
+    .header('WWW-Authenticate', 'Basic realm="Access to protected resource"')
+    .status(401)
+    .send({ error: 'Unauthorized' });
+};
+
+const replyProtectedResource = (reply: FastifyReply): FastifyReply => {
+  // Symbolic object representing a protected resource
+  return reply
+    .status(200)
+    .send({ protected_resource: 'protected_resource_data' });
+};
--- src/credentials.ts
+++ src/credentials.ts
@@ -1,17 +1,42 @@
+import { verify } from 'argon2';
+
 // Simple in memory credentials to simulate database
 
-interface BasicAuthCredentials {
+export interface BasicAuthCredentials {
   username: string;
   password: string;
 }
 
+// For demonstration purposes, the passwords have been hashed like they would before storing them in a production database. Argon2 was used to hash them and will be used to verify them by the authentication methods. Always use well established algorithms, never implement your own hashing algorithm.
 export const basicCredentials: Array<BasicAuthCredentials> = [
   {
     username: 'user1',
-    password: 'pwd1',
+    password:
+      '$argon2id$v=19$m=65536,t=3,p=4$trBDsXSkXsWNpN1dbNqgFQ$NruY20F74W9DEJn0O+A+gQIbp10R0QYB1AaJOiNq4O0',
   },
   {
     username: 'user2',
-    password: 'pwd2',
+    password:
+      '$argon2id$v=19$m=65536,t=3,p=4$RfRWlmnK7wbw1yvuEwIJ3A$wll9MtIAY9Jyyi+mgJ97z6Sk7/ZcRcvwrxjfYOFbA/o',
   },
 ];
+
+const verifyPassword = async (password: string, hashedPassword: string) => {
+  return verify(hashedPassword, password);
+};
+
+export const isCredentialsValid = async (
+  credentials: BasicAuthCredentials,
+): Promise<boolean> => {
+  for (const dbCredentials of basicCredentials) {
+    if (dbCredentials.username === credentials.username) {
+      const ok = await verifyPassword(
+        credentials.password,
+        dbCredentials.password,
+      );
+
+      if (ok) return true;
+    }
+  }
+  return false;
+};
